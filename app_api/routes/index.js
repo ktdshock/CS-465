f043d76e6a0faf288c8da22101f9068dbed08a31
Ken Dandrow <Kenneth.dandrow@snhu.edu>
2025-06-15 21:00:09 -0400

    Tested API endpoints with Postman and updated trip data

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const tripsController = require('../controllers/trips');
 
-console.log("app_api router loaded"); // <- put this here
+console.log("app_api router loaded");
 
 router.get('/trips', tripsController.tripsList);
 router.get('/trips/:tripCode', tripsController.tripsFindByCode);
+router.post('/trips', tripsController.tripsCreate);
+router.put('/trips/:tripCode', tripsController.tripsUpdate);
+router.delete('/trips/:tripCode', tripsController.tripsDelete);
+
 router.get('/ping', (req, res) => {
   res.status(200).json({ message: "pong" });
 });
